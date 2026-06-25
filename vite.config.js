0d10c24e8b258863572e4bfcbcbc9e02bbb41cec
Shivam Kumar <shivamkumar@example.com>
2026-06-25 07:36:16 +0530

    Revert outDir in vite config so Vercel can build to dist properly

--- vite.config.js
+++ vite.config.js
@@ -9,10 +9,6 @@ const __dirname = path.dirname(__filename)
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  build: {
-    outDir: '../Workden3.0',
-    emptyOutDir: true
-  },
   plugins: [
     react(),
   ],
